e717c35e6fa8651768d32fba831b623fa020bdb1
finboy97 <finbar201@gmail.com>
2023-12-03 14:03:47 +0000

    Test passes, input doesnt

--- source/day_03/day_03.py
+++ source/day_03/day_03.py
@@ -37,8 +37,72 @@ def part_01(list_of_str):
                 
     return total_parts_number
                 
+asterisk = re.compile("(\*)")
 
+def part_02(list_of_str):
+    height = len(list_of_str)
+    total_gear_ratio = 0
+    
+    for i, line in enumerate(list_of_str):
+        length = len(line)
+        matches=re.finditer(asterisk, line)
+       # print(i)
+        for run,match in enumerate(matches):
+            match_span = match.span()
+            start = 0 if match_span[0]< 2 else match_span[0]-1
+            end = length-1 if (match_span[1] == length-1) else match_span[1]+1
+            numbers_to_add = []
+            #Check current line
+            numbers_on_line=re.finditer(num_regex, line)
+            if numbers_on_line != None:
+                for number in numbers_on_line:
+                    number_range = range(number.span()[0], number.span()[1])
+                    num_adjacent=False
+                    if (start in number_range) or (end in number_range):
+                        num_adjacent=True
+                    if num_adjacent:
+                        #print(f"Adjacent_number {number.group()}")
+                        numbers_to_add.append(number.group())
+                        
+            #check previous line
+            if i > 0:
+                numbers_above = re.finditer(num_regex, list_of_str[i-1])
+                for number in numbers_above:
+                    number_range = range(number.span()[0], number.span()[1])
+                    num_adjacent=False
+                    for value in range(start, end):
+                        if value in number_range:
+                            num_adjacent=True
+                    if num_adjacent:
+                       # print(f"number above: {number.group()}")
+                        numbers_to_add.append(number.group())
+                    
+            #check next line
+            if i < height-1:
+                numbers_below = re.finditer(num_regex, list_of_str[i+1])
+                for number in numbers_below:
+                    #print(number.group())
+                    number_range = range(number.span()[0], number.span()[1])
+                    #print(number_range)
+                    num_adjacent=False
+                    #print(range(start,end))
+                    for value in range(start, end):
+                        #print(value in range(start,end))
+                        if value in number_range:
+                            num_adjacent=True
+                    if num_adjacent:
+                        #print(f"Number below: {number.group()}")
+                        numbers_to_add.append(number.group())
+
+            if len(numbers_to_add)==2:
+                #print(numbers_to_add)
+                total_gear_ratio+=(int(numbers_to_add[0])*int(numbers_to_add[1]))
+                
+    return total_gear_ratio
+                
+    
 
 with open("source/day_03/input") as input_file:
     input_list = input_file.read().split("\n")
     print(part_01(input_list))
+    print(part_02(input_list))
--- test/day_03/test_day_03.py
+++ test/day_03/test_day_03.py
@@ -1,4 +1,4 @@
-from source.day_03.day_03 import part_01
+from source.day_03.day_03 import part_01, part_02
 
 test_input = """467..114..
 ...*......
@@ -11,9 +11,12 @@ test_input = """467..114..
 ...$.*....
 .664.598.."""
 
+
 def test_part_01():
     input_list = test_input.split("\n")
     assert part_01(input_list) == 4361
 
+
 def test_part_02():
-    pass
+    input_list = test_input.split("\n")
+    assert part_02(input_list) == 467835
