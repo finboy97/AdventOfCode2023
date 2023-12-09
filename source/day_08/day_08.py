e452d8dfa6ceffa7684f9852742baa09a6e9d65c
Finbar Evans <finbar@kfwxl64hp9.home>
2023-12-09 12:22:29 +0000

    Finished day 8 part 2

--- source/day_08/day_08.py
+++ source/day_08/day_08.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import re
+import numpy as np
 
 test_input = """LLR
 
@@ -35,24 +36,35 @@ current_instruction = 0
 steps = 0
 z_reached = False
 
+z_steps = []
+for num in range(0,len(simultaneous_nodes)):
+    z_steps.append([])
 
+for i, value in enumerate(simultaneous_nodes):
+    current_value = value
+    circle_completed = False
+    current_instruction = 0
+    steps = 0
+    first_z = None
+    while not circle_completed:
+        steps += 1
+        if instructions[current_instruction] == "L":
+            current_value = location_map[current_value][0]
+        else:
+            current_value = location_map[current_value][1]
+        current_instruction += 1
+        if current_value[-1] == "Z":
+            if first_z is None:
+                first_z = current_value
+                z_steps[i].append(steps)
+                steps = 0
+            elif current_value == first_z:
+                z_steps[i].append(steps)
+                circle_completed = True
+        if current_instruction == len(instructions):
+            current_instruction = 0
 
-while not z_reached:
-    for i, value in enumerate(simultaneous_nodes):
-        if value[-1] != "Z":
-            if instructions[current_instruction] == "L":
-                simultaneous_nodes[i] = location_map[value][0]
-            else:
-                simultaneous_nodes[i] = location_map[value][1]
-
-    z_reached = True
-    for value in simultaneous_nodes:
-        if value[-1] != "Z":
-            z_reached = False
-    current_instruction += 1
-    if current_instruction == len(instructions):
-        current_instruction = 0
-    steps += 1
-    print(simultaneous_nodes)
-
-print(steps)
+# Didnt know how to do least common denominator so had to look it up. Found a numpy implementation
+z_steps = [max(x) for x in z_steps]
+lcm = np.lcm.reduce(z_steps)
+print(lcm)
